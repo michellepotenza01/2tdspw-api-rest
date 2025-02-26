b480941a45e3b144845ee369496161621a13ea1f
Luiz Gustavo S. Real <lgsreal2@gmail.com>
2025-02-25 22:27:37 -0300

    aula 25-02-2025

--- src/main/java/br/com/fiap/api_rest/controller/LivroController.java
+++ src/main/java/br/com/fiap/api_rest/controller/LivroController.java
@@ -15,9 +15,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = "/livros")
@@ -37,9 +38,10 @@ public class LivroController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<LivroResponse>> readLivros() {
+    public ResponseEntity<Page<LivroResponse>> readLivros(@RequestParam(defaultValue = "0") Integer pageNumber) {
         Pageable pageable = PageRequest
-                .of(0, 2, Sort.by("titulo").ascending());
+                .of(pageNumber, 2, Sort.by("autor").ascending()
+                        .and(Sort.by("titulo").ascending()));
         //Page<Livro> livros = livroRepository.findAll(pageable);
         return new ResponseEntity<>(livroService.findAll(pageable),HttpStatus.OK);
     }
@@ -52,7 +54,14 @@ public class LivroController {
         if (livro.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(livroService.livroToResponse(livro.get()),HttpStatus.OK);
+        LivroResponse livroResponse = livroService.livroToResponse(livro.get());
+        livroResponse.setLink(
+                linkTo(
+                        methodOn(LivroController.class)
+                                .readLivros(0)
+                ).withRel("Lista de Livros")
+        );
+        return new ResponseEntity<>(livroResponse,HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
--- src/main/java/br/com/fiap/api_rest/dto/LivroResponse.java
+++ src/main/java/br/com/fiap/api_rest/dto/LivroResponse.java
@@ -1,4 +1,28 @@
 package br.com.fiap.api_rest.dto;
 
-public record LivroResponse(String infoLivro) {
+import org.springframework.hateoas.Link;
+
+public class LivroResponse {
+    private String infoLivro;
+    private Link link;
+
+    public LivroResponse(String infoLivro) {
+        this.infoLivro = infoLivro;
+    }
+
+    public String getInfoLivro() {
+        return infoLivro;
+    }
+
+    public void setInfoLivro(String infoLivro) {
+        this.infoLivro = infoLivro;
+    }
+
+    public Link getLink() {
+        return link;
+    }
+
+    public void setLink(Link link) {
+        this.link = link;
+    }
 }
