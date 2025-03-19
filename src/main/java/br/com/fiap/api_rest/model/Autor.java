929a333ec0c0df42f1d3b325d698b300017d713a
michellepotenza01 <rm557702@fiap.com.br>
2025-03-18 23:19:39 -0300

    Adicionando alterações

---
+++ src/main/java/br/com/fiap/api_rest/model/Autor.java
@@ -0,0 +1,42 @@
+package br.com.fiap.api_rest.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @ManyToMany
+    @JoinTable(name = "autor_livro",
+            joinColumns = @JoinColumn(name = "id_livro", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "id_autor", referencedColumnName = "id"))
+    private List<Livro> livros;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+}
